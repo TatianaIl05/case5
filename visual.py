91a1e8501b47fc1fa1e099891ebd279c90ea17f9
Tania <t.ilinykh1@g.nsu.ru>
2024-04-09 22:46:30 +0700

    add changes to battle and reconcile funcs

--- visual.py
+++ visual.py
@@ -2,13 +2,15 @@ import pygame
 from random import randint
 from prettytable import PrettyTable
 
-food = [2000, 2000, 2000, 2000]
+food = [14000, 14000, 14000, 14000]
 people = [7000, 7000, 7000, 7000]
 findings = [0, 0, 0, 0]
 research_tools = [20, 20, 20, 20]
 defense = [50, 50, 50, 50]
 
 pygame.init()
+screen = pygame.display.set_mode((600, 600))
+background = pygame.image.load('space_background.png')
 
 
 class Player:
@@ -79,12 +81,6 @@ players = [team_1, team_2, team_3, team_4]
 square_colors = ['red', 'yellow', 'green', 'blue']
 trace_colors = ['pink', 'white', 'light green', 'light blue']
 
-screen = pygame.display.set_mode((600, 600))
-background = pygame.image.load('space_background.png')
-
-running = True
-current_team = 1
-
 
 def alien_invasion(team):
     probability = randint(1, 100)/100
@@ -94,70 +90,83 @@ def alien_invasion(team):
         print('Нападение инопланетных рас')
         people[players.index(team)] *= 0.9
         defense[players.index(team)] *= 0.9
-        print(team.x, team.y)
-        print(people[players.index(team)], defense[players.index(team)])
+        print(f'Экипаж: {people[players.index(team)]}, Средства защиты: {defense[players.index(team)]}')
 
 
 def monthly_food(team):
     food[players.index(team)] += 1000
     food[players.index(team)] -= people[players.index(team)]//4
-    print(food[players.index(team)])
+    return food[players.index(team)]
 
 
-rival_teams = [0, 0, 0, 0]
+rival_teams = [0] * 12
 
 
 def question():
-    global current_team
-    if input('У Вас есть возможность взаимодействовать с другими командами. '
-             'Хотите ли Вы бросить кому-то вызов? ').lower() == 'да':
+    global current_team, j
+    if input('Хотите ли Вы бросить кому-то вызов? ').lower() == 'да':
         compete_team = int(input('Введите номер команды, с которой вы будете соревноваться: '))
         while compete_team == current_team or compete_team > 4:
             print('Неверное значение')
             compete_team = int(input('Введите другое значение: '))
-        rival_teams[compete_team - 1] = current_team
+        rival_teams[current_team - 1 + 4*j] = compete_team
         print(f'Команда {current_team} бросила вызов команде {compete_team}')
 
 
 def answer():
-    global current_team
-    if rival_teams[current_team - 1] != 0:
-        if input(f'Согласна ли команда {current_team} на противостояние с'
-                 f' командой {rival_teams[current_team - 1]}? ').lower() == 'да':
-            print('Противостояние')
-            battle()
-        else:
-            print('Отказ от противостояния')
-            reconcile()
+    global current_team, j
+    for k in range(len(rival_teams)):
+        if rival_teams[k] == current_team:
+            if input(f'Согласна ли команда {current_team} на противостояние с'
+                     f' командой {int((k+1) % 4) if (k+1) % 4 != 0 else int((k+5)/(j+1))}? ').lower() == 'да':
+                print('Противостояние')
+                battle()
+            else:
+                print('Отказ от противостояния')
+                reconcile()
 
 
 def battle():
+    global j
     num_ask = randint(1, 100)
     num_accept = randint(1, 100)
-    print(f'team_1: {num_ask}, team_2: {num_accept}')
+    print(f'Команда, бросившая вызов: {num_ask}, Вторая команда: {num_accept}')
     if num_ask > num_accept:
-        food[rival_teams[current_team - 1] - 1] += 0.5*food[current_team - 1]
+        food[rival_teams.index(current_team) - 4*j] += 0.5*food[current_team - 1]
         food[current_team - 1] *= 0.5
     elif num_ask < num_accept:
-        food[current_team - 1] += 0.5 * food[rival_teams[current_team - 1] - 1]
-        food[rival_teams[current_team - 1] - 1] *= 0.5
+        food[current_team - 1] += 0.5 * food[rival_teams.index(current_team) - 4*j]
+        food[rival_teams.index(current_team) - 4*j] *= 0.5
     else:
         food[current_team - 1] += 50
-        food[rival_teams[current_team - 1] - 1] += 50
+        food[rival_teams.index(current_team) - 4*j] += 50
+    print(f'Ресурсы команд равны: {food[rival_teams.index(current_team) - 4*j]} и {food[current_team - 1]}')
+    rival_teams[rival_teams.index(current_team)] = 0
 
 
 def reconcile():
+    global j
     food[current_team - 1] *= 0.8
-    people[rival_teams[current_team - 1] - 1] *= 0.8
+    people[rival_teams.index(current_team) - 4*j] *= 0.8
+    print(f'Ресурсы команд равны: {people[rival_teams.index(current_team) - 4*j]} и {food[current_team - 1]}')
+    rival_teams[rival_teams.index(current_team)] = 0
 
 
+running = True
+current_team = 1
 count_times = 0
-# for i in range(7): count_times == 0+i, 1+i, 2+i, 3+i
+j = 0
+
 while running:
     bar_1.draw()
     bar_2.draw()
     bar_3.draw()
     bar_4.draw()
+    if j > 2:
+        running = False
+        print('Игра окончена!!!')
+    if 0 in people:
+        people[people.index(0)] = 100
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
@@ -171,18 +180,23 @@ while running:
             case 1:
                 team_1.do(square_colors[0], trace_colors[0])
                 if count_times == 0:
+                    print('Ход первой команды')
+                    print('За этот месяц во время путешествия произошло несколько событий: ')
+                    print(f'Изменение ресурса - пропитание {monthly_food(team_1)}')
                     alien_invasion(team_1)
-                    monthly_food(team_1)
-                    answer() if count_times != 0 else print('Пока никто не бросил вызов')
+                    print('Взаимодействия команд: ')
+                    answer()
                     question()
                     bar_1.hp = people[0]/100
                     count_times += 1
-
             case 2:
                 team_2.do(square_colors[1], trace_colors[1])
                 if count_times == 1:
+                    print('Ход второй команды')
+                    print('За этот месяц во время путешествия произошло несколько событий: ')
                     alien_invasion(team_2)
-                    monthly_food(team_2)
+                    print(f'Изменение ресурса - пропитание {monthly_food(team_2)}')
+                    print('Взаимодействия команд: ')
                     answer()
                     question()
                     bar_2.hp = people[1] / 100
@@ -190,8 +204,11 @@ while running:
             case 3:
                 team_3.do(square_colors[2], trace_colors[2])
                 if count_times == 2:
+                    print('Ход третьей команды')
+                    print('За этот месяц во время путешествия произошло несколько событий: ')
+                    print(f'Изменение ресурса - пропитание {monthly_food(team_3)}')
                     alien_invasion(team_3)
-                    monthly_food(team_3)
+                    print('Взаимодействия команд: ')
                     answer()
                     question()
                     bar_3.hp = people[2] / 100
@@ -199,12 +216,14 @@ while running:
             case 4:
                 team_4.do(square_colors[3], trace_colors[3])
                 if count_times == 3:
+                    print('Ход четвёртой команды')
+                    print('За этот месяц во время путешествия произошло несколько событий: ')
+                    print(f'Изменение ресурса - пропитание {monthly_food(team_4)}')
                     alien_invasion(team_4)
-                    monthly_food(team_4)
+                    print('Взаимодействия команд: ')
                     answer()
                     question()
                     bar_4.hp = people[3] / 100
-                    rival_teams[0], rival_teams[1], rival_teams[2] = 0, 0, 0
                     count_times = 0
                     resource_table = PrettyTable()
                     resource_table.add_column('Команды', [1, 2, 3, 4])
@@ -214,13 +233,13 @@ while running:
                     resource_table.add_column('Средства защиты', defense)
                     resource_table.add_column('Средства изучения', research_tools)
                     print(resource_table)
+                    j += 1
 
         for i in range(1, 4 + 1):
             if i != current_team:
                 pygame.draw.rect(screen, square_colors[i - 1], pygame.Rect(players[i - 1].x, players[i - 1].y, 10, 10))
                 for point in players[i - 1].trace_points:
                     pygame.draw.rect(screen, trace_colors[i - 1], pygame.Rect(point[0], point[1], 1, 1))
-    pygame.draw.circle(screen, 'blue', (270, 430), 10)
     pygame.display.update()
 
 pygame.quit()
