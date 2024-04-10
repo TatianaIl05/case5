7fbf6feb82f05323d9f02b66c1fc2c63c9104e12
ShiJiliana <ulgulakova@yandex.ru>
2024-04-10 11:15:38 +0700

    changes

--- METEOR_RAIN.py
+++ METEOR_RAIN.py
@@ -7,24 +7,24 @@ def case_if_check():
     if avaliable_meteor_check == '1':
         match meteor_chance:
             case 1:
-                print('There is a meteor danger')
+                print('Существует метеоритная опасность')
                 case_if_stay_go()
             case 2:
-                print('There is no meteor danger')
+                print('Метеоритной опасности нет')
                 case_if_stay_go()
     else:
-        print('The weather is too bad to analise')
+        print('Погода слишком плохая, чтобы анализировать ситуацию')
         case_if_stay_go()
 
 
 def case_if_stay_go():
-    print('Do you want to stay there or go to another place?', '\n', 'press 1 - stay, press 2 - go', '\n')
+    print('Вы хотите остаться там или поехать в другое место?', '\n', 'нажмите 1 - остаться, нажмите 2 - перейти', '\n')
     match int(input()):
         case 1:
-            print('Despite the risk you stayed')
+            print('Несмотря на риск, ты остался')
             return 1
         case 2:
-            print('You have gone to another place. God protects those who are careful')
+            print('Вы ушли в другое место. Бог хранит тех, кто осторожен')
             return 2
         case _:
             print('Неверно введено значение')
@@ -32,7 +32,7 @@ def case_if_stay_go():
 
 
 def aware():
-    print('Do you want to check meteor activity?', '\n', 'press 1 - yes, press 2 - no')
+    print('Вы хотите проверить метеорную активность?', '\n', 'нажмите 1 - да, нажмите 2 - нет')
     a = str(input())
     global meteor_chance
     meteor_chance = random.randint(1, 2)
@@ -42,7 +42,7 @@ def aware():
         print('хозяин - барин')
         return case_if_stay_go()
     else:
-        print('wrong value')
+        print('неправильное значение')
         aware()
 
 
@@ -58,10 +58,10 @@ def meteor_rain(defence, stuff):
             elif defence == 0:
                 stuff = stuff // 3
         else:
-            print('The meteor rain have ran other side. Lucky you')
+            print('Метеоритный дождь прошел с другой стороны. Повезло тебе')
         return [defence, stuff]
     elif (aware_ == 2 and int(meteor_chance) == 2) or (aware_ is None and int(meteor_chance) == 2):
-        print(f'You stayed on location, what will be next?')
+        print(f'Вы остались на месте, что будет дальше?')
         return [defence, stuff]
     # elif aware == None and int(meteor_chance) == 1:
 
