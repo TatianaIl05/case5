7ca73c25829297ecc310e224367056e8f0476e8e
Getto2asher <109324161+Getto2asher@users.noreply.github.com>
2024-04-15 16:04:02 +0700

    Main

--- STARVE.py
+++ STARVE.py
@@ -1,26 +1,26 @@
-def rep_func(x, y, z):
-    if x == y:
-        return True
-    if x == z:
-        return False
+# def rep_func(x, y, z):
+#     if x == y:
+#         return True
+#     if x == z:
+#         return False
+#     else:
+#         print('wrong value')
+#         rep_func(x, y, z)
+#
+#
+# def check_food(food):
+#     print('Check food?', '\n', '1 - yes | 2 - no')
+#     var = int(input())
+#     ans = rep_func(var, 1, 2)
+#     if ans:
+#         print(f'you have {food} food before step')
+
+
+def starve(food, people, usage):
+    # check_food(food)
+    if usage > food:
+        print(f'Starvation. You have lack of food. You lost {int(abs(food - usage))} people')
+        return [0, people - abs(food - usage), (people - usage) * 0.5]
     else:
-        print('wrong value')
-        rep_func(x, y, z)
-
-
-def check_food(food):
-    print('Check food?', '\n', '1 - yes | 2 - no')
-    var = int(input())
-    ans = rep_func(var, 1, 2)
-    if ans:
-        print(f'you have {food} food before step')
-
-
-def starve(food, people):
-    check_food(food)
-    if people * 0.5 > food:
-        print(f'Starvation. You have lack of food. You lost {int(abs(food - people * 0.5))} people')
-        return [0, int(people - abs(food - people))]
-    else:
-        return [int(food - people * 0.5), people]
+        return [food - usage, people, people * 0.5]
 
--- main.py
+++ main.py
@@ -1,17 +1,26 @@
 from prettytable import PrettyTable
+import STARVE
 
-food = [14000, 14000, 14000, 14000]
+food = [10000, 10000, 10000, 10000]
 people = [7000, 7000, 7000, 7000]
 attack = [100, 100, 100, 100]
-research_tools = [20, 20, 20, 20]
+# usage = [people[0] * 0.5, people[1]*0.5, people[2]*0.5, people[3]*0.5]
+usage = [3500, 3500, 3500, 3500]
 defense = [50, 50, 50, 50]
+
+
 def start_info():
-    print(f'Ход {step + 1} команды {team + 1}')
-    table_columns = ['Люди', 'Еда', 'Защита', 'Атака']
+    print(f'\n Ход {step + 1} команды {team + 1}')
+    food[team], people[team], usage[team] = STARVE.starve(food[team], people[team], usage[team])
+    print('Данные на начало хода: ')
+    table_columns = ['Люди', 'Еда', 'Защита', 'Атака', 'Потребление']
     table = PrettyTable(table_columns)
-    table.add_row([people[team], food[team], defense[team], attack[team]])
+    table.add_row([people[team], food[team], defense[team], attack[team], usage[team]])
     print(table)
 
+
 for step in range(4):
-    for team in range(4):
+    for team in range(1):
         start_info()
+
+
