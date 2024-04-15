c3970e29769a1b510648f2054c3182a7688c2d58
Getto2asher <109324161+Getto2asher@users.noreply.github.com>
2024-04-15 17:20:01 +0700

    Main

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 from prettytable import PrettyTable
 import STARVE
 import METEOR_RAIN
+import CHECK
+import case
 
 food = [10000, 10000, 10000, 10000]
 people = [7000, 7000, 7000, 7000]
-attack = [100, 100, 100, 100]
-usage = [people[0] * 0.5, people[1]*0.5, people[2]*0.5, people[3]*0.5]
+ar = [1, 1, 1, 1]
+attack = [int(people[0] * 0.3) * ar[0], int(people[1] * 0.3) * ar[1], int(people[2] * 0.3) * ar[2],
+          int(people[3] * 0.3) * ar[3]]
+usage = [people[0] * 0.5, people[1] * 0.5, people[2] * 0.5, people[3] * 0.5]
 defense = [5000, 5000, 5000, 5000]
 
 
 def start_info():
-    print(f'\n Ход {step + 1} команды {team + 1}')
     food[team], people[team], usage[team] = STARVE.starve(food[team], people[team], usage[team])
     print('Данные на начало хода: ')
     table_columns = ['Люди', 'Еда', 'Защита', 'Атака', 'Потребление']
@@ -20,8 +23,13 @@ def start_info():
 
 
 for step in range(7):
-    for team in range(4):
-        start_info()
+    for team in range(1):
+        print(f'\n Ход {step + 1} команды {team + 1}')
         people[team], defense[team] = METEOR_RAIN.meteor_rain(people[team], defense[team])
-
-
+        start_info()
+        people[team], ar[team], defense[team], flag = case.case(people[team], ar[team], attack[team],
+                                                                defense[team], flag=None)
+        attack[team] = int(people[team] * 0.3 * ar[team])
+        if flag == 'red':
+            print('Кому хотите объявить войну?')
+        # CHECK.check_war()
