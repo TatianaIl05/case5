5dd1cc7d9663cb26596d888b54c48d46f8e707c1
ShiJiliana <ulgulakova@yandex.ru>
2024-04-17 01:14:06 +0700

    changes

--- METEOR_RAIN.py
+++ METEOR_RAIN.py
@@ -1,4 +1,6 @@
 import random
+import ru_local
+
 def meteor_rain(people, defense):
     '''
     This function triggers a random event in the form of a meteorite impact.
@@ -9,10 +11,10 @@ def meteor_rain(people, defense):
     meteor_chance = random.randint(1, 3)
     if meteor_chance == 1:
         if defense < 2000:
-            print(f'На вас вас упал метеорит! Вы потеряли {people - int(people * 0.7)} людей.')
+            print(f'{ru_local.METEORITE} {people - int(people * 0.7)} {ru_local.PEOPLES}')
             return [int(people * 0.7), 0]
         else:
-            print(f'На вас упал метеорит! Вы потеряли {defense * 0.5} защиты.')
+            print(f'{ru_local.METEORITE} {defense * 0.5} {ru_local.DEFENSES2}')
             return [people, defense * 0.5]
     else:
         return [people, defense]
