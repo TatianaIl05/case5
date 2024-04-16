6f7817f8daf38ec36e9d9fe3626aea3588a136d4
Getto2asher <109324161+Getto2asher@users.noreply.github.com>
2024-04-16 23:19:17 +0700

    Main

--- case.py
+++ case.py
@@ -14,7 +14,7 @@ def case(people, ar, attack, defense, food, flag=None):
             flag = 'red'
             return [people, ar, defense, food, flag]
         case 4:
-            print(f'Вы купили еду. Теперь у вас {food + 8000}')
+            print(f'Вы купили еду. Теперь у вас {food + 8000} еды')
             return [people, ar, defense, food + 8000, flag]
         case _:
             print('Неверное значение')
