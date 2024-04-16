90df288f5197d6972011d5d1c557eeea83aa399d
Tania <t.ilinykh1@g.nsu.ru>
2024-04-16 18:28:44 +0700

    add fun

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from prettytable import PrettyTable
+from random import randint
 import STARVE
 import METEOR_RAIN
 import CHECK
 import case
 import random_events as ran
+import ru_local as ru
 
 food = [10000, 10000, 10000, 10000]
 people = [7000, 7000, 7000, 7000]
@@ -25,16 +27,65 @@ def start_info():
     print(table)
 
 
+rival_teams = [0] * 12
+
+
+def question():
+    global team, step
+    if input(ru.DESIRE_CHALLENGE).lower() == ru.YES_NO:
+        compete_team = int(input(ru.NUM_TEAM))
+        while compete_team == team or compete_team > 4:
+            print(ru.VALUE_ERROR)
+            compete_team = int(input(ru.OTHER_VALUE))
+        rival_teams[team - 1 + 4*step] = compete_team
+        print(ru.TEAM, team, ru.CHALLENGED, compete_team)
+
+
+def answer():
+    global team, step
+    for k in range(len(rival_teams)):
+        if rival_teams[k] == team:
+            if input(f'{ru.APPROVAL_TEAM} {team} {ru.BATTLE} '
+                     f'{int((k+1) % 4) if (k+1) % 4 != 0 else 4}').lower() == ru.YES_NO:
+                print(ru.FIGHT)
+                battle()
+            else:
+                print(ru.NOT_FIGHT)
+                reconcile()
+
+
+def battle():
+    global step
+    num_ask = randint(1, 100)
+    num_accept = randint(1, 100)
+    print(ru.TEAM_CHELLENGE, num_ask, ru.SECOND_TEAM, num_accept)
+    if num_ask > num_accept:
+        food[rival_teams.index(team) - 4*step] += 0.5*food[team - 1]
+        food[team - 1] *= 0.5
+    elif num_ask < num_accept:
+        food[team - 1] += 0.5 * food[rival_teams.index(team) - 4*step]
+        food[rival_teams.index(team) - 4*step] *= 0.5
+    else:
+        food[team - 1] += 50
+        food[rival_teams.index(team) - 4*step] += 50
+    print(ru.RESOURCES_EQUAL, food[rival_teams.index(team) - 4*step],  food[team - 1])
+    rival_teams[rival_teams.index(team)] = 0
+
+
+def reconcile():
+    global step
+    food[team - 1] *= 0.8
+    people[rival_teams.index(team) - 4*step] *= 0.8
+    print(ru.RESOURCES_EQUAL, people[rival_teams.index(team) - 4*step], food[team - 1])
+    rival_teams[rival_teams.index(team)] = 0
+
+
 for step in range(3):
-    for team in range(3):
-        print(f'\n Ход {step + 1} команды {team + 1}')
-        people[team], defense[team] = METEOR_RAIN.meteor_rain(people[team], defense[team])
+    for team in range(1, 4 + 1):
+        print(f'\n Ход {step + 1} команды {team}')
+        people[team - 1], defense[team - 1] = METEOR_RAIN.meteor_rain(people[team - 1], defense[team - 1])
         start_info()
-        ran.sickness(people[team])
-        ran.artifacts(findings[team], research_tools[team])
-        people[team], ar[team], defense[team], flag = case.case(people[team], ar[team], attack[team],
-                                                                defense[team], flag=None)
-        attack[team] = int(people[team] * 0.3 * ar[team])
-        if flag == 'red':
-            print('Кому хотите объявить войну?')
-        # CHECK.check_war()
+        ran.sickness(people[team - 1])
+        ran.artifacts(findings[team - 1], research_tools[team - 1])
+        people[team - 1], ar[team - 1], defense[team - 1], flag = case.case(people[team - 1], ar[team - 1], attack[team - 1], defense[team - 1], flag=None)
+        attack[team - 1] = int(people[team - 1] * 0.3 * ar[team - 1])
